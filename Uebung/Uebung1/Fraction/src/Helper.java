f28cb4ec7e3e2430d9f35eddc0406c963e149919
Niklas Büschelmann <niklas.bueschelmann@web.de>
2025-10-15 17:46:35 +0200

    Uebung1 Fertig

--- Uebung/Uebung1/Fraction/src/Helper.java
+++ Uebung/Uebung1/Fraction/src/Helper.java
@@ -1,22 +1,9 @@
 public class Helper {
-
     public static int ggT(int a, int b) {
-        if (a < 0 ){ a = -a;}
-        if (b < 0) { b = -b;}
-
-        // Komische Fehler abgreifen
-        if(a == 0 && b == 0){return 1;}
-        if(a == 0){return b;}
-        if(b == 0){return a;}
-        
-
-        while (a != b) {
-            if (b > a) {
-                int temp = b;
-                b = a;
-                a = temp;
-            }
-            a = a - b;
+        while (b != 0) {
+            int rest = a % b;
+            a = b;
+            b = rest;
         }
         return a;
     }
