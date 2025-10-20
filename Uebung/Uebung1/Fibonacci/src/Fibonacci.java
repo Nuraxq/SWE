047a3ccdde9cadbcbef29095795dac80e386a116
Niklas Büschelmann <niklas.bueschelmann@web.de>
2025-10-20 09:03:14 +0200

    Fibonacci mit Fehler

--- Uebung/Uebung1/Fibonacci/src/Fibonacci.java
+++ Uebung/Uebung1/Fibonacci/src/Fibonacci.java
@@ -1,20 +1,32 @@
+import java.util.InputMismatchException;
+
 public class Fibonacci {
     public static void main(String[] args) throws Exception {
         
-        fib(10);
+        fib(-1);
     }
 
 
     public static void fib(int n) {
-        int a = 0;
-        int b = 1;
-
-        while(n > 1){
-            int temp = b;
-            b = a+b;
-            a = temp;
-            n--;
-        }
-        System.out.println("Die gesuchte fibonacci Zahl lautet: " + b);
+        try{
+            if(n < 0){ throw new InputMismatchException("Negative n sind nicht erlaubt!");}
+            int a = 0;
+            int b = 1;
+            if(n <= 1)
+            {
+                System.out.println("Die gesuchte fibonacci Zahl lautet: " + n); 
+                return;
+            }
+            while(n > 1){
+                int temp = b;
+                b = a+b;
+                a = temp;
+                n--;
+            }
+            System.out.println("Die gesuchte fibonacci Zahl lautet: " + b);
+            }catch(InputMismatchException e)
+            {
+                System.out.println(e.getMessage());
+            }
     }
 }
